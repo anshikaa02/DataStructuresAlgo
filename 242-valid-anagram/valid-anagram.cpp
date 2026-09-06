726c57f7af9d011d9353c772c8baf1fec8e37c14
Anshika Sharma <anshika.sharma0027@gmail.com>
2026-09-06 23:16:45 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort (t.begin(), t.end());
-        if (s==t) return true;
-        return false;
+        unordered_map< char, int> map;
+        for (int i=0; i<s.size(); i++){
+            char ch= s[i];
+            map[ch]++;
+        }
+        for (int i=0; i<t.size(); i++){
+            char ch= t[i];
+            map[ch]--;
+        }
+        for (char ch='a'; ch<='z'; ch++){
+
+        if (map[ch]!=0) return false;
+        }
+        return true;
+        
     }
 };
